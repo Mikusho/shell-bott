76d73bc06ae9728433251646a9803df2e53355c2
Mikusho <50960187+Mikusho@users.noreply.github.com>
2022-07-04 01:51:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -343,9 +343,8 @@ bot.command("up", function (msg, reply, next) {
       var command = msg.context.command;
       return reply.reply(command.initialMessage.id || msg).html("Can't change directory while a command is running.");
     }
-    var up = require('shelljs');
-    try {
-      up.exec('./up %arg');
+    var shell = require('shelljs');
+    shell.sh('up');
     } catch (err) {
       return reply.html("%s", err);
     }
