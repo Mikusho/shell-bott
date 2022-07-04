ba8617514132f5d4aa17878f88154f52e0f52326
Mikusho <50960187+Mikusho@users.noreply.github.com>
2022-07-04 22:17:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -341,8 +341,6 @@ bot.command("up", function (msg, reply, next) {
     var upload = require('shelljs');
     upload.exec('./up' + " " + arg);
     upload.echo(arg);
-
-  reply.html("Berhasil Upload ke DUMP!");
   });
 });
 
